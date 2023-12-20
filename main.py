513f43dba3868e3765eac2eb9e6964df19ecf3aa
Hassan Kashif <rhkashif@gmail.com>
2023-12-20 20:55:34 +0000

    writing parsing vote function

--- main.py
+++ main.py
@@ -7,22 +7,11 @@ It reads data from a supplied file, which is updated throughout the night as res
 Author: Hassan Kashif
 """
 
-PARTY_CODES = {
-    "C": "Conservative Party", 
-    "L": "Labour Party",
-    "UKIP": "UKIP", 
-    "LD": "Liberal Democrats", 
-    "G": "Green Party", 
-    "Ind": "Independent", 
-    "SNP": "SNP"}
 
-def load_data(filename: str):
+
+def load_data(filename: str) -> list:
     """
-    Load election data from a given text file.
-
     This function reads an election results file, where each line represents a constituency's election result.
-    The file format is expected to have comma-separated values with the constituency name followed by repeating 
-    pairs of party codes and the votes cast for each party.
 
     Parameters:
     filename (str): The path to the election results text file.
@@ -39,8 +28,79 @@ def load_data(filename: str):
     except IOError as e:
         raise IOError(f"Error reading file {filename}: {e}")
 
+def parse_election_results(line: str, error_log: list) -> dict:
+    """
+    Each line contains a constituency name followed by party-vote pairs. This function
+    extracts these details and returns a structured dictionary.
+
+    Parameters:
+    line (str): A line from the election results file.
+
+    Returns:
+    dict: A dictionary with the constituency name and a dictionary of party-vote pairs.
+    """
+    elements = line.strip().split(', ')
+    constituency = elements.pop(0)
+    votes = {}
+
+    for i in range(1, len(elements)-1, 2):
+        party = elements[i]
+        try:
+            vote_count = int(elements[i + 1])
+            if vote_count > 0:
+                votes[party] = vote_count
+            else:
+                votes[party] = 0
+        except ValueError:
+            error_log.append(f"Invalid vote count for party '{party}' in line: {line}")
+    
+    return {"constituency": constituency, "votes": votes}
+
+def calculate_vote_shares(votes: dict):
+    """
+    Calculates the vote share of each party in a constituency.
+
+    Parameters:
+    votes (dict): A dictionary of party-vote pairs.
+
+    Returns:
+    dict: A dictionary with parties and their vote share percentages.
+    """
+    total_votes = sum(votes.values())
+    vote_shares = {party: (votes[party] / total_votes) * 100 for party in votes}
+    return vote_shares
+
+
 
 if __name__ == "__main__":
     DATA_FILE_NAME = "sample_results.txt"
+
+    PARTY_CODES = {
+        "C": "Conservative Party", 
+        "L": "Labour Party",
+        "UKIP": "UKIP", 
+        "LD": "Liberal Democrats", 
+        "G": "Green Party", 
+        "Ind": "Independent", 
+        "SNP": "SNP"}
+
+    PARTY_VOTES = {
+        "Conservative Party": 0, 
+        "Labour Party": 0,
+        "UKIP": 0, 
+        "Liberal Democrats": 0, 
+        "Green Party": 0, 
+        "Independent": 0, 
+        "SNP": 0} 
+
     ELECTION_DATA = load_data(DATA_FILE_NAME)
+
+    ERROR_LOG = []
+
+    for line in ELECTION_DATA:
+        parsed_votes = parse_election_results(line, ERROR_LOG)
+        print(parsed_votes)
+        # vote_shares = calculate_vote_shares(parsed_votes)
+        # print(vote_shares)
+
     
